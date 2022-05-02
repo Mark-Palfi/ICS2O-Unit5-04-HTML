cf0a77feb0554903cd76f50ea4a21ec31793c700
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-05-02 19:39:48 +0000

    edits

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function enterClicked() {
   const age = parseInt(document.getElementById("age").value)
 
   //process
-  if ((age >= 12 && age <= 21) || (day=="Tuesday" || day=="Thursday") {
+  if ((age >= 12 && age <= 21) || (day=="Tuesday" || day=="Thursday")) {
     document.getElementById("price").innerHTML = "You get the student discount"
   }  else {
     document.getElementById("price").innerHTML = "You need to pat regualer price"
